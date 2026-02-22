677af96962476957b64ec0421e4553410e3484ed
Gerry-01 <odhiambogerry010@gmail.com>
2026-02-22 17:14:09 +0300

    added the urls

--- social_media/urls.py
+++ social_media/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('complete_profile/', views.complete_profile, name='complete_profile'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('create_post/', views.create_post, name='create_post'),
+    path('profile/', views.profile, name='profile'),
 ]
